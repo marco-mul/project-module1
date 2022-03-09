d9fedca5a5d308d25af87e0cd8a1d501f6cf8237
Marco Muller <marco.muller.mm@gmail.com>
2022-03-09 10:52:12 +0100

    improve HTML and CSS

--- script.js
+++ script.js
@@ -1,35 +1,73 @@
 // Game functionality
 
-// Function startGame
+// Starting the game
+
+let score = 0
+
+let time = 60
+
+let divToWhack = document.querySelectorAll('.putin')
+console.log(divToWhack)
+
+buttonStart = document.getElementById('my-button')
+console.log(buttonStart)
+
+gameBoard = document.getElementById('game')
+console.log(gameBoard)
+
+gameStartScreen = document.getElementById('no-game')
+console.log(gameStartScreen)
 
 
 
+function startGame() {
+    buttonStart.innerText = 'Restart';
+    gameStartScreen.classList.remove('no-game');
+    gameBoard.classList.remove('hidden');
+    divToWhack.forEach(item => {
+    item.src="./img/Putin.png"
+     })
+     score = 0;
+     scoreScreen.innerText = score;
+     time = 60;
+     timeScreen.innerText = time
+}
+
+buttonStart.addEventListener('click', startGame)
 
 
 // Time
 
-time = 60
-
 let timeScreen = document.getElementById('time-screen')
 console.log(timeScreen)
+
 
 timeScreen.innerText = time
 
 // Score 
 
-let score = 25
+
+function addPoints(a) {
+    score = score + a;
+    return score
+}
+
+
 let scoreScreen = document.getElementById('score-screen')
 console.log(scoreScreen)
 
+console.log(score)
+
 scoreScreen.innerText = score
 
-let divToWhack = document.querySelectorAll('.hole-up')
-console.log(divToWhack)
+// Playing the game
 
 
-/*divToWhack.forEach(element => element.addEventListener('click', function(){
-    divToWhack.classList.add('putin-dead')
-}));
-*/
-
-
+divToWhack.forEach(item => {
+    item.addEventListener('click', event => {
+        console.log(item)
+        item.src="./img/Putin-dead.png" ;
+        addPoints(10);
+        scoreScreen.innerText = score;
+        })
+    });
