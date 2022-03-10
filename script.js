952365aff30396574c64f6534db0e625afd2ea20
Marco Muller <marco.muller.mm@gmail.com>
2022-03-10 11:18:26 +0100

    add game end scenarios, restart game  function, haptic feedback

--- script.js
+++ script.js
@@ -17,50 +17,87 @@ let time = 60
 let divToWhack = document.querySelectorAll('.putin')
 console.log(divToWhack)
 
-buttonStart = document.getElementById('my-button')
+let buttonStart = document.getElementById('my-button')
 console.log(buttonStart)
 
-gameBoard = document.getElementById('game')
+let gameBoard = document.getElementById('game')
 console.log(gameBoard)
 
-gameStartScreen = document.getElementById('no-game')
+let gameStartScreen = document.getElementById('no-game')
 console.log(gameStartScreen)
 
+let gameWinScreen = document.getElementById('win-game')
+console.log(gameWinScreen)
+
+let gameLooseScreen = document.getElementById('loose-game')
+console.log(gameLooseScreen)
+
 let timeInterval
+let randomPutin
+let oldRandomPutin
+
+let randomPutinInt
+let oldRandomPutinInt
 
 
 function startGame() {
     buttonStart.innerText = 'Restart';
     gameStartScreen.classList.remove('no-game');
+    gameWinScreen.classList.add('hidden');
+    gameWinScreen.classList.remove('win-game');
+    gameLooseScreen.classList.add('hidden');
+    gameLooseScreen.classList.remove('loose-game');
     gameBoard.classList.remove('hidden');
     divToWhack.forEach(item => {
     item.src="./img/Putin.png"
      })
      
+    randomPutinInt = setInterval(function() {
+        const index = Math.floor(Math.random() * (0 - divToWhack.length + 1)) + divToWhack.length;
+        randomPutin = divToWhack[index]
+        randomPutin.classList.add('visible')
+        oldRandomPutin = randomPutin
+    }, 2005);
+
+
+    oldRandomPutinInt = setInterval(function() {
+        oldRandomPutin.classList.remove('visible')
+    }, 1995);
+
      score = 0;
      scoreScreen.innerText = score;
      time = 60;
-     timeScreen.innerText = time
+     timeScreen.innerText = time;
      timeInterval = setInterval(function() {
         time = time-1;
         timeScreen.innerText = time
         if (time === 0) {
-          clearInterval(timeInterval)
+          clearInterval(timeInterval);
+          clearInterval(randomPutinInt);
+          clearInterval(oldRandomPutinInt)
+          gameBoard.classList.add('hidden');
+          endGame();
       }}, 1000);
 }
 
-// function winGame()
+function winGame(){
+    gameWinScreen.classList.remove('hidden');
+    gameWinScreen.classList.add('win-game');
+}
 
-// function loseGame()
+function loseGame(){
+    gameLooseScreen.classList.remove('hidden');
+    gameLooseScreen.classList.add('loose-game');
+}
 
 
-// function endGame(){
-//     if (score>=250) {
-//         winGame()
-//     } else {
-//         loseGame()
-//     }
-// }
+function endGame(){
+    if (score >= 250) {
+        winGame()
+    } else {
+        loseGame()
+    }
+}
 
 
 function restartGame() {
@@ -73,24 +110,24 @@ buttonStart.addEventListener('click', restartGame)
 
 // select random divs to show
 
-let randomPutin
-let oldRandomPutin
+// let randomPutin
+// let oldRandomPutin
 
-let randomPutinInt
-let oldRandomPutinInt
-
-
-randomPutinInt = setInterval(function() {
-    const index = Math.floor(Math.random() * (0 - divToWhack.length + 1)) + divToWhack.length;
-    randomPutin = divToWhack[index]
-    randomPutin.classList.add('visible')
-    oldRandomPutin = randomPutin
-  }, 2005);
+// let randomPutinInt
+// let oldRandomPutinInt
 
 
-oldRandomPutinInt = setInterval(function() {
-    oldRandomPutin.classList.remove('visible')
-  }, 1995);
+// randomPutinInt = setInterval(function() {
+//     const index = Math.floor(Math.random() * (0 - divToWhack.length + 1)) + divToWhack.length;
+//     randomPutin = divToWhack[index]
+//     randomPutin.classList.add('visible')
+//     oldRandomPutin = randomPutin
+//   }, 2005);
+
+
+// oldRandomPutinInt = setInterval(function() {
+//     oldRandomPutin.classList.remove('visible')
+//   }, 1995);
 
 
 // Time display
@@ -127,6 +164,7 @@ divToWhack.forEach(item => {
         console.log(item)
         item.src="./img/Putin-dead.png" ;
         document.getElementById('hidden-sound').play();
+        navigator.vibrate(50);
         addPoints(10);
         scoreScreen.innerText = score;
         setTimeout(function() {
